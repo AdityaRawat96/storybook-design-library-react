ea967b8976f9ac8f3083ed6dee92a4f5bfd18058
Aditya Rawat <adityasinghrawat96@gmail.com>
2024-08-05 05:20:57 +0530

    Code coverage tests added

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,10 +7,15 @@ const config = {
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-coverage',
   ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
+  },
+  docs: {
+    autodocs: 'tag',
   },
 };
 export default config;
