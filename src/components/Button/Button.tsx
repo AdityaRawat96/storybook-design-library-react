730cdb11b7358bdd14f18bfcf1663a9e06bc000a
Aditya Rawat <adityasinghrawat96@gmail.com>
2024-08-09 09:43:46 +0530

    Fixed bugs related to TS support

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -23,7 +23,7 @@ const useStyles = makeStyles({
   buttonSecondary: {},
 });
 
-const Button = ({ variant, ...props }) => {
+const Button = ({ variant, ...props }: ButtonProps) => {
   const classes = useStyles();
 
   return (
